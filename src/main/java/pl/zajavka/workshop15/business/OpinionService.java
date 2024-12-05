700ffadda22cb50ccbc35c4f1a80801821f0d5fb
Pawel9481 <pawel.demichowicz@interia.pl>
2024-12-05 12:16:18 +0100

    Refactor code

--- src/main/java/pl/zajavka/workshop15/business/OpinionService.java
+++ src/main/java/pl/zajavka/workshop15/business/OpinionService.java
@@ -3,7 +3,6 @@ package pl.zajavka.workshop15.business;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import pl.zajavka.workshop15.domain.Customer;
 import pl.zajavka.workshop15.domain.Opinion;
 
 import java.util.List;
